0b9ee8fd127c94e89b69e7075e13ce8864716c52
Yura <104792276+Yura-111@users.noreply.github.com>
2022-12-22 14:54:38 +0200

    Add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import { Contacts } from 'components/contacts/Contacts';
@@ -8,83 +8,55 @@ import {Box, BoxForm, Title} from 'components/App.styled'
 
 const LOKAL_KEY = 'my-contacts';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  
+    const [contacts, setContacts] = useState([]);
+    const [filter, setFilter] = useState(''); 
+  
+    useEffect(() => {
+      window.localStorage.setItem(LOKAL_KEY, JSON.stringify(contacts));
+    }, [contacts]);
 
-  componentDidMount() {
-    const friends = JSON.parse(localStorage.getItem(LOKAL_KEY));
+    const AddFriend = (event, { resetForm }) => {
+      const contactName = [];
+      const friendId = nanoid();
+  
+      for (const contact of contacts) {
+        contactName.push(contact.name.toLowerCase());
+      }
+  
+      if (contactName.includes(event.name.toLowerCase())) {
+        alert(`${event.name} is already in contacts list`);
+        return;
+      }
+      const user = { ...event, id: friendId };
+      setContacts(prev => [user, ...prev]);
+      resetForm();
+    };
+  
 
-    if (friends) {
-      this.setState({
-        contacts: friends,
-      });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (prevState.contact !== this.state.contacts) {
-      localStorage.setItem(LOKAL_KEY, JSON.stringify(this.state.contacts));
-    }
-  }
-
-  onSubmit = ({ name, number }, { resetForm }) => {
-    const friendId = nanoid();
-    const findedContact = this.state.contacts.find(contact =>
-      contact.name.toLowerCase().includes(name.toLowerCase())
+    const onDelite = friendId => {
+      setContacts(contacts.filter(contact => contact.id !== friendId));
+    };
+  
+    const normalizedName = filter.toLowerCase();
+  
+    const visContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedName)
     );
-
-    if (findedContact) {
-      alert(`${findedContact.name} is already in contacts`);
-      return;
-    } else {
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, { id: friendId, name, number }],
-      }));
-    }
-
-    resetForm();
-  };
-
-  filterChange = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  getVisContacts = () => {
-    const { contacts } = this.state;
-    const filterNormalized = this.state.filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filterNormalized)
-    );
-  };
-
-  deliteContact = id => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(contact => contact.id !== id),
-      };
-    });
-  };
-
-  render() {
-    const { onSubmit, filterChange, deliteContact } = this;
-    const { contacts, filter } = this.state;
-    const visContacts = this.getVisContacts();
+  
 
     return (
       <Box>
         <Title>Phonebook</Title>
         <BoxForm>
-          <Form onSubmit={onSubmit} />
+          <Form onSubmit={AddFriend} />
         </BoxForm>
           <h3>Contacts</h3>
-            <Filter value={filter} onChange={filterChange} />
+            <Filter value={filter} onChange={evt => setFilter(evt.currentTarget.value)} />
             {contacts.length > 0 && (
-              <Contacts contacts={visContacts} onDelite={deliteContact} />
+              <Contacts contacts={visContacts} onDelite={onDelite} />
             )}
       </Box>
     );
   }
-}
